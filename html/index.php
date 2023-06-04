5fdb122f767e34922d20b08be5c0387765da008e
Dev365TH <116702680+ottoP789@users.noreply.github.com>
2023-06-04 15:41:23 +0700

    master

---
+++ html/index.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Admin Template</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+Jqnj1O4dYCvb1Xy3T/w2TaJwGXUpgI" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet">
+  </head>
+  <body>
+
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+      <a class="navbar-brand" href="#">Admin Template</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Dashboard</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Orders</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Customers</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Products</a>
+          </li>
+        </ul>
+        <form class="form-inline mt-2 mt-md-0">
+          <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
+
+    <main role="main" class="container-fluid">
+      <div class="row">
+        <div class="col-md-3">
+          <div class="list-group">
+            <a href="#" class="list-group-item list-group-item-action active">Dashboard</a>
+            <a href="#" class="list-group-item list-group-item-action">Orders</a>
+            <a href="#" class="list-group-item list-group-item-action">Customers</a>
+            <a href="#" class="list-group-item list-group-item-action">Products</a>
+          </div>
+        </div>
+        <div class="col-md-9">
+          <h1>Dashboard</h1>
+          <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec facilisis odio nec dolor consectetur fermentum. Donec commodo tortor eget dui ornare pharetra.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum faucibus nisl arcu, eget pulvinar tellus tristique non. Nunc ac lacinia felis. Nulla facilisi. In commodo ultricies orci, sed consectetur augue posuere sed. Maecenas lobortis velit odio, et euismod ante sodales eget. Ut eget dui sed mauris auctor dignissim id at ligula. Aenean at laoreet tellus, quis molestie libero. Vivamus tempus ex at metus maximus fermentum. Proin vel risus quis urna consectetur dignissim a nec quam.</p>
+        </div>
+      </div>
+    </main>
+
+    <!-- Bootstrap JavaScript -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+Jqnj1O4dYCvb1Xy3T/w2TaJwGXUpgI" crossorigin="anonymous"></script>
+  </body>
+</html>
